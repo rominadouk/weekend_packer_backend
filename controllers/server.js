b8399ea6ca47e9b75f11181d527ea99fae5cdaf3
rominadouk <douk.romina@gmail.com>
2023-12-11 20:26:48 -0800

    cors error handling to allow access from all origins

--- controllers/server.js
+++ controllers/server.js
@@ -9,6 +9,13 @@ require('dotenv').config();
 //middleware
 app.use(express.json());
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*'); //allows access from any origin
+    // res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 //Mongo DB Load
 const port = process.env.PORT || 4000
